31798e5c1238b8804c314bd3c0eceafc1186745a
Ezhik <Ezhikdoma95538@mail.ru>
2023-06-25 12:06:54 +0500

    ДЗ для 4-го семинара

---
+++ Задача 25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// ==========================НАЧАЛО==========================
+System.Console.WriteLine("Число A");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Число B");
+int B = Convert.ToInt32(Console.ReadLine());
+int product = 1;
+for (int i = 0; i < B;)
+{
+    product = product * A;
+    i = i + 1;
+    System.Console.WriteLine(product);
+}
+// ==========================КОНЕЦ==========================
+
---
+++ Задача 29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] FillArray(int leftRange, int rightRange)
+{
+    int[] tempArray = new int[8];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        tempArray[i] = rand.Next(leftRange, rightRange + 1);
+    }
+
+    return tempArray;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+}
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+// ------------------------------------------------------------------
+int[] array=FillArray(ReadInt("Введите левую границу массива"), ReadInt("Введите правую границу массива"));
+PrintArray(array);
